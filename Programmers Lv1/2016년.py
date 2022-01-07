df5f49113003929b75569dce1f8dcfb86edf7a56
Haram0111 <5gkfka5@naver.com>
2022-01-08 00:26:42 +0900

    22.01.08

--- Programmers Lv1/2016년.py
+++ Programmers Lv1/2016년.py
@@ -4,9 +4,6 @@ def solution(a, b):
     monthday = [31,29,31,30,31,30,31,31,30,31,30,31]
     for i in range(a-1):
         sum += monthday[i]
-    sum = (sum+b) % 7
-    index = 5 + sum
-    if index > 6:
-        index -= 7
-    return day[index-1]
+    sum = (sum+b-1) % 7
+    return day[sum-2]
 #왜 틀렸는지 모르겠음
