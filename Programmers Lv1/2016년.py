365d33d961b31770775734f9d6088c96a3c57701
Haram0111 <5gkfka5@naver.com>
2022-04-29 15:08:43 +0900

    2022.04.29

--- Programmers Lv1/2016년.py
+++ Programmers Lv1/2016년.py
@@ -1,9 +1,8 @@
 def solution(a, b):
     sum = 0
-    day = ["SUN","MON","TUE","WEN","THU","FRI","SAT"]
+    day = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
     monthday = [31,29,31,30,31,30,31,31,30,31,30,31]
     for i in range(a-1):
         sum += monthday[i]
     sum = (sum+b-1) % 7
     return day[sum-2]
-#왜 틀렸는지 모르겠음
