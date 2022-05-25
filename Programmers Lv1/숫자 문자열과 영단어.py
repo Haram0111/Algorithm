f9ced685b5615e801b45684bf116c991e279a183
Haram0111 <5gkfka5@naver.com>
2022-05-25 12:48:20 +0900

    22.05.25

--- Programmers Lv1/숫자 문자열과 영단어.py
+++ Programmers Lv1/숫자 문자열과 영단어.py
@@ -1,3 +1,14 @@
 def solution(s):
-    answer = 0
-    return answer
+    result = ""
+    s = list(s)
+    st = ["ze","on","tw","th","fo","fi","si","se","ei","ni"]
+    num = ["0","1","2","3","4","5","6","7","8","9"]
+    print(s)
+    for i in range(len(s)-1):
+        if s[i] in num:
+            result += s[i]
+        if s[i]+s[i+1] in st:
+            result += num[st.index(s[i]+s[i+1])]
+    if s[-1] in num:
+        result += s[-1]
+    return int(result)
