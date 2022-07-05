4180f60aa19cb056ec9c5978285afcef539607b9
Haram0111 <5gkfka5@naver.com>
2022-07-06 00:01:29 +0900

    22.07.05

--- Programmers Lv2/튜플.py
+++ Programmers Lv2/튜플.py
@@ -1,15 +1,11 @@
 def solution(s):
     answer = []
-    s = s[1:-1]
-    s = s.split('}')
-    s.sort(key=len)
-    print(s)
-    for i in range(1, len(s)):
-        for j in range(1, len(s[i])):
-            if s[i][j] != ',' and s[i][j] != '{':
-                if s[i][j] not in answer:
-                    answer.append(s[i][j])
-    for i in range(len(answer)):
-        answer[i] = int(answer[i])
+    s = s[2:-2]
+    s = s.split("},{")
+    s.sort(key = len)
+    for i in s:
+        ii = i.split(',')
+        for j in ii:
+            if int(j) not in answer:
+                answer.append(int(j))
     return answer
-        
