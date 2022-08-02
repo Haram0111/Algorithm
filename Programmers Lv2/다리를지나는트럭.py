1fcb799a7062491827a457cff10f2e7363f995f3
Haram0111 <5gkfka5@naver.com>
2022-08-02 23:39:15 +0900

    22.08.02

--- Programmers Lv2/다리를지나는트럭.py
+++ Programmers Lv2/다리를지나는트럭.py
@@ -4,18 +4,16 @@ def solution(bridge_length, weight, truck_weights):
     On_bridge = deque()
     cnt = 0
     truck_weights = deque(truck_weights)
-    for i in range((weight+1)*len(truck_weights)):
+    sum_weights = 0
+    while 1:
         cnt += 1
-        sum_weights = 0
         if len(On_bridge) >= bridge_length: #들어가 있는 숫자의 개수가 브릿지의 길이보다 길면
-            On_bridge.popleft() #제일 처음 들어온 값을 내보낸다
-        for j in On_bridge: #현제 브릿지에 올라가있는 트럭의 무게를 구한다
-            sum_weights += j 
+            sum_weights -= On_bridge.popleft() #제일 처음 들어온 값을 내보낸다
         if sum_weights + truck_weights[0] <= weight: #만약 전체 합과 다음에 올라올 트럭의 무게의 합이 최대중량보다 작다면
             if len(truck_weights) == 1: #또한, 대기중인 트럭의 길이가 1이라면
-                #truck_weights.popleft() #마지막 것을 꺼내준다
-                break
-            On_bridge.append(truck_weights.popleft()) #대기중인 트럭의 길이가 1이 아니라면 그냥 꺼낸다
+                return cnt + bridge_length #마지막 것을 꺼내준다
+            truck = truck_weights.popleft()
+            On_bridge.append(truck) #대기중인 트럭의 길이가 1이 아니라면 그냥 꺼낸다
+            sum_weights +=  truck
         else: #최대중량을 초과한다면
             On_bridge.append(0) #최대중량이라면
-    return cnt + bridge_length
