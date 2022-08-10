47e3e4c39f90e7b33ba33b6e9d9892f7b0f18233
Haram0111 <5gkfka5@naver.com>
2022-08-11 06:23:48 +0900

    Not Solved

--- Programmers Lv2/행렬의 곱셈.py
+++ Programmers Lv2/행렬의 곱셈.py
@@ -1,14 +1,11 @@
-arr1 = [[1, 4], [3, 2], [4, 1]]
-arr2 = 	[[3, 3], [3, 3]]
-answer = []
-for i in arr1:
-    for j in i:
-        for k in range(len(arr2[0])):
-            print(k)
-        print(j)
-    print("------")
-#    for j in range(len(arr1[i])):
-#        for k in range(len(arr2[j])):
-#            answer.append(arr1[i][j] * arr2[k][j])
-print(answer)
-#[[15, 15], [15, 15], [15, 15]]
+arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
+arr2 = 	[[5, 4, 3], [2, 4, 1], [3, 1, 1]]
+lst = [[] for _ in range(len(arr1))]
+for i in range(len(arr1)): #3번 줄반복
+    for j in range(len(arr1[i])): #2번반복
+        sum = 0
+        for k in range(len(arr2[0])): #2번 반복
+            a = arr1[i][k] * arr2[k][j]
+            sum += a
+        lst[i].append(sum)
+print(lst)
