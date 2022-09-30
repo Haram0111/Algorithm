cdc521af88c16287dc5c8324fa89ca53e3983d0f
Haram0111 <5gkfka5@naver.com>
2022-09-30 10:04:48 +0900

    22.09.30

--- Programmers Lv2/행렬의 곱셈.py
+++ Programmers Lv2/행렬의 곱셈.py
@@ -1,11 +1,7 @@
-arr1 = [[2, 3, 2], [4, 2, 4], [3, 1, 4]]
-arr2 = 	[[5, 4, 3], [2, 4, 1], [3, 1, 1]]
-lst = [[] for _ in range(len(arr1))]
-for i in range(len(arr1)): #3번 줄반복
-    for j in range(len(arr1[i])): #2번반복
-        sum = 0
-        for k in range(len(arr2[0])): #2번 반복
-            a = arr1[i][k] * arr2[k][j]
-            sum += a
-        lst[i].append(sum)
-print(lst)
+import numpy as np
+def solution(arr1, arr2):
+    arr1 = np.array(arr1)
+    arr2 = np.array(arr2)
+    arr = arr1 @ arr2
+    arr = arr.tolist()
+    return arr
