7550a202c046b0983e5b7ec5b7a6903db70feb9e
Haram0111 <5gkfka5@naver.com>
2023-01-22 20:08:35 +0900

    23.01.22

--- Programmers Lv2/할인행사.py
+++ Programmers Lv2/할인행사.py
@@ -1,1 +1,23 @@
-print("할인행사!!")
+def solution(want, number, discount):
+    dic = {}
+    answer = 0
+    for i in range(len(discount)):
+        for k in range(len(want)):
+            dic[want[k]] = 0
+        if i + 10 <= len(discount) - 1:
+            for j in range(10):
+                if discount[i+j] in dic:
+                    dic[discount[i+j]] += 1
+        else:
+            for j in range(len(discount) - i):
+                if discount[i+j] in dic:
+                    dic[discount[i+j]] += 1
+        result = list(dic.values())
+        count = 0
+        for m in range(len(number)):
+            if number[m] == result[m]:
+                count += 1
+        if count == len(number):
+            answer += 1
+    return answer
+            
