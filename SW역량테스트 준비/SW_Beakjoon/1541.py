cb1711bb5632d5ea4be278d3b4a658ef50ba1241
Haram0111 <5gkfka5@naver.com>
2024-01-06 19:08:08 +0900

    Greedy

--- SW역량테스트 준비/SW_Beakjoon/1541.py
+++ SW역량테스트 준비/SW_Beakjoon/1541.py
@@ -1,6 +1,17 @@
-num = list(input())
-num = num.split("+")
+num = input()
 num = num.split("-")
-num = num.split("/")
-num = num.split("*")
-print(num)
+
+answer = []
+
+for i in num:
+    a = 0
+    tmp = i.split("+")
+    for j in tmp:
+        a += int(j)
+    answer.append(a)
+
+n = answer[0]
+
+for i in range(1,len(answer)):
+    n -= answer[i]
+print(n)
