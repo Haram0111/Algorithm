96abbf26bf3fbcc36b1a0c6f1667d79781c9f5c0
Haram0111 <5gkfka5@naver.com>
2024-01-10 14:26:38 +0900

    DFS/BFS

--- SW역량테스트 준비/SW_Beakjoon/11725.py
+++ SW역량테스트 준비/SW_Beakjoon/11725.py
@@ -1,12 +1,12 @@
 import sys
 sys.setrecursionlimit(10**6)
 
-def DFS(graph, start, visited, root):
+def dfs(graph, start, visited, root):
     visited[start] = root
     root = start
     for i in graph[start]:
         if visited[i] == 0:
-            DFS(graph, i, visited, start)
+            dfs(graph, i, visited, start)
 
 N = int(input())
 graph = [ [] for i in range(N+1) ]
@@ -15,6 +15,6 @@ for i in range(N-1):
     start, end = map(int,input().split(" "))
     graph[start].append(end)
     graph[end].append(start)
-DFS(graph, 1, visited, 0)
+dfs(graph, 1, visited, 0)
 for i in visited[2:]:
     print(i)
